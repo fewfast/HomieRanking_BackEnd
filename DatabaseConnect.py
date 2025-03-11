daf4f5d21885bef48705c4add30bae018c15e096
Sinpoom <sinpoom2547@gmail.com>
2025-03-11 06:35:20 +0000

    test env

--- DatabaseConnect.py
+++ DatabaseConnect.py
@@ -2,15 +2,16 @@ from flask import Flask, render_template
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://HomieRanking:9NZWKcQB4HWCdjUc@mydb.uxw7p.mongodb.net/?retryWrites=true&w=majority&appName=mydb"
-
+uri = os.getenv("MONGO_URI")
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
+db = client.get_database()
 
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
+    print("Collections:", db.list_collection_names())
 except Exception as e:
     print(e)
 
@@ -19,3 +20,4 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+    
